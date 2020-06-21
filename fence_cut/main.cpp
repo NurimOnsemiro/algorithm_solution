c5342f903575ff3c0ce2c46a46ab2e25c77d4f42
gshong <hgs33hgs@gmail.com>
2020-06-21 12:53:03 +0900

    울타리 문제 코드 추가

---
+++ fence_cut/main.cpp
@@ -0,0 +1,20 @@
+/**
+* https://www.algospot.com/judge/problem/read/FENCE
+* °á°ú: 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int numTests{ 0 };
+	scanf("%d", &numTests);
+
+	while (numTests > 0) {
+		
+		numTests--;
+	}
+
+	return 0;
+}
