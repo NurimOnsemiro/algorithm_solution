03c45e57e3e1128e5bae6f502ae59a2809e8642f
gshong <hgs33hgs@gmail.com>
2020-06-22 18:24:54 +0900

    nodejs 정답

--- fence_cut/nodejs/index.ts
+++ fence_cut/nodejs/index.ts
@@ -1,6 +1,6 @@
 /**
  * https://www.algospot.com/judge/problem/read/FENCE
- * 결과: ms
+ * 결과: 208ms
  */
 
 import * as readline from 'readline';
@@ -13,49 +13,126 @@ let rl: readline.Interface = readline.createInterface({
 
 let input: string[] = [];
 
-class game_manager{
-  temp: Buffer;
-  buffer: Buffer;
+class stack_base {
+  data: number[];
+  private top: number;
 
   constructor()
   {
-    this.buffer = Buffer.from([]);
-    this.temp = Buffer.from([]);
+    this.data = [];
+    this.data.length = 20001;
+    this.top = 0;
+    this.data[this.top] = 0;
   }
 
-  input_data(data: string): void
+  push(value: number)
   {
-    this.buffer = Buffer.from(data);
-    this.temp = Buffer.from(data);
+    if(this.top === this.data.length - 1){
+      return;
+    }
+    this.top++;
+    this.data[this.top] = value;
   }
 
-  find_max_size_fence(): number
+  front(): number
   {
-    
+    if(this.top === 0){
+      throw new Error("stack is empty");
+    }
+    return this.data[this.top];
   }
 
-  find_answer(): void
+  pop()
   {
-    
+    if(this.top === 0){
+      throw new Error("stack is empty");
+    }
+    this.top--;
+  }
+
+  empty(): boolean
+  {
+    if(this.top === 0) return true;
+    else return false;
+  }
+}
+
+class game_manager{
+  fenceArr: number[];
+  numFences: number;
+  maxSquareSize: number;
+
+  constructor()
+  {
+    this.numFences = 0;
+    this.maxSquareSize = 0;
+    this.fenceArr = [];
+  }
+
+  clear_data()
+  {
+    this.fenceArr = [];
+    this.fenceArr.length = 20001;
+    this.numFences = 0;
+    this.maxSquareSize = 0;
+  }
+
+  input_data(fenceStrArr: string[])
+  {
+    for(let i=0;i<fenceStrArr.length;i++){
+      this.fenceArr[i] = Number(fenceStrArr[i]);
+    }
+    this.fenceArr[fenceStrArr.length] = 0;
+    this.numFences = fenceStrArr.length;
+  }
+
+  find_answer()
+  {
+    let stk: stack_base = new stack_base();
+    this.fenceArr[this.numFences] = 0;
+
+    for(let i=0;i<=this.numFences;i++){
+      while(!stk.empty() && this.fenceArr[stk.front()] >= this.fenceArr[i] ){
+        let pos: number = stk.front()
+        stk.pop();
+
+        let width: number = stk.empty() ? i : (i - stk.front() - 1);
+        let squareSize: number = width * this.fenceArr[pos];
+        if(squareSize > this.maxSquareSize){
+          this.maxSquareSize = squareSize;
+        }
+      }
+      stk.push(i);
+    }
   }
 
   output_data()
   {
-    console.log(this.buffer.toString());
+    console.log(this.maxSquareSize);
   }
 }
 
 function main(): void
 {
   let numTests: number = 0;
-  numTests = Number(input[0]);
+  let idx: number = 0;
+  numTests = Number(input[idx++]);
 
   let gm: game_manager = new game_manager();
+  
 
-  for(let i = 1; i <= numTests; i++){
-    gm.input_data(input[i]);
+  while(numTests){
+    let numFences: number = Number(input[idx++]);
+
+    let fecneDataStr: string = input[idx++];
+    let fenceArr: string[] = fecneDataStr.split(' ');
+
+    gm.clear_data();
+    gm.input_data(fenceArr);
     gm.find_answer();
     gm.output_data();
+
+    numTests--;
   }
 }
 
